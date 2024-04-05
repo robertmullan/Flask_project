a0ad5421778be2667029e48f69d08beb23e6fe2f
Hunter Gibson <114520043+huntergibson@users.noreply.github.com>
2024-04-04 19:41:29 -0500

    completed steps 10-19

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
